c8106352777a5062693458ff4328345a844e244a
Yuichi Ishida <yuqrecord@gmail.com>
2025-10-05 19:57:48 +0900

    change: プラグインのバージョン更新とTreesitter設定の修正 - lazy-lock.jsonの複数プラグインのコミットIDを更新 - nvim-treesitterの設定にbranch指定とignore_install追加

--- nvim/lua/plugins/syntax/nvim-treesitter.lua
+++ nvim/lua/plugins/syntax/nvim-treesitter.lua
@@ -1,13 +1,15 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	config = function () 
+	config = function ()
 		local configs = require("nvim-treesitter.configs")
 		configs.setup({
+      branch = "master",
 			ensure_installed = "all",
+      ignore_install = { "ipkg" },
 			sync_install = false,
 			highlight = { enable = true },
-			indent = { enable = true },  
+			indent = { enable = true },
 			additional_vim_regex_highlighting = false,
 		})
 	end
